5650dc9df9940bcbbe4ca8b816afc87964e4cdfa
Tabia Nadir <nadirumer786001@gmail.com>
2024-12-16 16:50:56 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
-    return (
-      <>
-        <h1 className='text-2xl font-bold mb-5'>Welcome</h1>
-        <p className='mb-5'>
-          This is the demo site for Traversy Media's Next.js & Clerk tutorial. Go
-          ahead and sign up or sign in!
-        </p>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-5">Welcome</h1>
+      <p className="mb-5">
+        This is the demo site for Traversy Media&apos;s Next.js &amp; Clerk tutorial. Go
+        ahead and sign up or sign in!
+      </p>
+    </>
+  );
+}
